3fd1d0928e2e65bb2009bbc4306b85096407d864
Pelle Pelster <pelle@pelle.io>
2013-12-16 23:27:27 +0100

    added junit tests to build

---
+++ io.pelle.webexample/test/io/pelle/webexample/WebExampleTestSuite.java
@@ -0,0 +1,11 @@
+package io.pelle.webexample;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)	
+@SuiteClasses({ UserDAOTest.class }) 
+public class WebExampleTestSuite {
+
+}
